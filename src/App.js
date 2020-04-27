d14bc80adcb5889be57140e18ddc76f3cb568080
Adrian <adrian_zinko@op.pl>
2020-04-27 20:27:53 +0200

    feat: create a layout component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import './App.css';
+import Layout from './components/Layout/Layout'
 
 function App() {
   return (
     <div>
+      <Layout>
+        <p>Test</p>
+      </Layout>
     </div>
   );
 }
