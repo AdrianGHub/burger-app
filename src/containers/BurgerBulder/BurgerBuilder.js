819e56c75e9e618af3036c2a3e969c7878b78a47
Adrian <adrian_zinko@op.pl>
2020-04-28 19:27:22 +0200

    feat: output burger components dynamically

--- src/containers/BurgerBulder/BurgerBuilder.js
+++ src/containers/BurgerBulder/BurgerBuilder.js
@@ -4,10 +4,22 @@ import Aux from '../../hoc/Auxiliary';
 import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            ingredients: {
+                salad: 1,
+                bacon: 1,
+                cheese: 1,
+                meat: 3
+            }
+        } 
+    }
+
     render() {
         return (
             <Aux>
-               <Burger />
+               <Burger ingredients={this.state.ingredients}/>
                 <div>Build Controls</div>
             </Aux>
         )
