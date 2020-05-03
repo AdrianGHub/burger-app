1daca31b6349f41ce69f6d8656454fec79adad10
Adrian <adrian_zinko@op.pl>
2020-05-03 13:29:16 +0200

    feat: add toolbar component with logo image

---
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import classes from '../Toolbar/Toolbar.module.css';
+import Logo from '../../Logo/Logo';
+
+const toolbar = () => {
+    return (
+        <header className={classes.Toolbar}>
+            <div>MENU</div>
+            <Logo />
+            <nav>
+                ...
+            </nav>
+        </header>
+    );
+};
+
+export default toolbar;
