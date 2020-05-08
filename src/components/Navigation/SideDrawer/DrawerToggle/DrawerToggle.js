34578d2514d722aea1523aaf2674c2c9696f5cde
Adrian <adrian_zinko@op.pl>
2020-05-08 12:30:14 +0200

    feat: implement hamburger menu

--- src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
+++ src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React from "react";
+import classes from "./DrawerToggle.module.css";
 
-const drawerToggle = props => {
-    return (
-        <div onClick={props.clicked}>
-            MENU
-        </div>
-    );
+const drawerToggle = (props) => {
+	return (
+		<div className={classes.DrawerToggle} onClick={props.clicked}>
+			<div></div>
+			<div></div>
+			<div></div>
+		</div>
+	);
 };
 
 export default drawerToggle;
