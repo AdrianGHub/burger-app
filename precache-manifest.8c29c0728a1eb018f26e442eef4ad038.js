3664a834899377902392ea34bdb259f66dd0166d
Adrian <adrian_zinko@op.pl>
2020-05-10 20:28:34 +0200

    Updates

---
+++ precache-manifest.8c29c0728a1eb018f26e442eef4ad038.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2887c1d71dcf918bc9e03bd72a717421",
+    "url": "/burger-app/index.html"
+  },
+  {
+    "revision": "6bac2620f944af9ff2e9",
+    "url": "/burger-app/static/css/main.ce0d4100.chunk.css"
+  },
+  {
+    "revision": "d8343c3c39318921d88a",
+    "url": "/burger-app/static/js/2.8fbbb7fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/burger-app/static/js/2.8fbbb7fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bac2620f944af9ff2e9",
+    "url": "/burger-app/static/js/main.a8030763.chunk.js"
+  },
+  {
+    "revision": "94ab0f792746b0690994",
+    "url": "/burger-app/static/js/runtime-main.7222db98.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger-app/static/media/burger-logo.b8503d26.png"
+  }
+]);
