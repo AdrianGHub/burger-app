f98bf11b3f91cb868c195b65fbdca341d218131d
Adrian <adrian_zinko@op.pl>
2020-06-08 19:00:31 +0200

    feat: create checkout component

---
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Burger from '../../Burger/Burger';
+import Button from '../../UI/Button/Button';
+import classes from './CheckoutSummary.module.css';
+
+const checkoutSummary = (props) => {
+    return (
+        <div className={classes.CheckoutSummary}>
+            <h1>We hope you won't develop diarrhea!</h1>
+            <div style={{width: "300px", height: '300px', margin: 'auto'}}>
+                <Burger ingredients={props.ingredients}/>
+            </div>
+            <Button 
+              buttonType="Danger"
+              clicked>CANCEL</Button>
+            <Button 
+              buttonType="Success"
+              clicked>CONTINUE</Button>
+            
+            
+        </div>
+    );
+};
+
+export default checkoutSummary;
