101185eba0d8e4e5b49310678d89b185257faa50
Adrian <adrian_zinko@op.pl>
2020-06-22 19:21:39 +0200

    feat: handle user input

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -11,6 +11,7 @@ const input = (props) => {
 					className={classes.InputElement}
 					{...props.elementConfig}
 					value={props.value}
+					onChange={props.changed}
 				/>
 			);
 			break;
@@ -20,6 +21,7 @@ const input = (props) => {
 					className={classes.InputElement}
 					{...props.elementConfig}
 					value={props.value}
+					onChange={props.changed}
 				/>
 			);
 			break;
@@ -27,7 +29,11 @@ const input = (props) => {
 			inputElement = (
 				<select className={classes.InputElement} value={props.value}>
 					{props.elementConfig.options.map((option) => (
-						<option key={option.value} value={option.value}>
+						<option
+							key={option.value}
+							value={option.value}
+							onChange={props.changed}
+						>
 							{option.displayValue}
 						</option>
 					))}
@@ -40,6 +46,7 @@ const input = (props) => {
 					className={classes.InputElement}
 					{...props.elementConfig}
 					value={props.value}
+					onChange={props.changed}
 				/>
 			);
 			break;
